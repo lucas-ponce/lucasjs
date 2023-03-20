98563361218a93a8c01bcbf02573f744eb864d9d
lucas ivan ponce <lucasivanponce@gmail.com>
2023-03-20 12:57:51 -0300

    Se agregaron nuevas funciones en el carrito de compra

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+//Creo la clase producto para que el cliente ingrese un producto ,se cree una instancia de objeto y se agregue a la lista carrito 
+class Producto {
+    constructor(nombre,precio){
+        this.nombre= nombre.toUpperCase();
+        this.precio= parseFloat(precio);
+    }
+}
+const carrito= [];
 //La funcion validar datos chequea si la entrada de dia , mes y hora es distinta a un numero 
 function validarDatos(dia, mes, hora) {
     let validar = true;
@@ -15,33 +23,65 @@ function validarDatos(dia, mes, hora) {
     }
     return validar;
 }
-//La funcion validar datos deja al cliente sumar productos y arroja el precio total 
+//La funcion comprarProductos() deja al cliente sumar productos mediante un push 
 function comprarProductos(){
-    let precioTotal = 0 ;
-    let producto;
-    while (producto != "s") {
-        producto = prompt("Ingresa el nombre del producto o 's' para salir");
-        if (producto != "s") {
+    let comprando;
+    while (comprando != "s") {
+        comprando = prompt("Ingresa el nombre del producto o 's' para salir");
+        if (comprando != "s") {
             let precio = parseFloat(prompt("Ingrese el precio del producto"));
             if (!isNaN(precio)) {
-                alert(`El precio de ${producto} es de : ${precio} ARS`);
-                precioTotal = precioTotal + precio;
+                nuevoProducto= new Producto(comprando,precio);
+                carrito.push(nuevoProducto);
+                alert(`El precio de ${comprando} es de : ${precio} ARS`);
             }
         }
     }
-return precioTotal;
+    console.log(carrito);
+}
+//La funcion total(carrito) nos permite entrar al precio de cada objeto y guardarlo en un contador para luego retornarlo  
+function total(carrito){
+    let cont = 0;
+    for (const producto of carrito){
+        cont = (cont + producto.precio);
+    }
+    return cont;
 }
 
-//La funcion pagocuotas permite al usuario elegir un plan de pago
-function pagocuotas(precioTotal , int ){
+//La funcion pagocuotas permite al usuario elegir un plan de pago en cuotas
+function pagocuotas(precioTotal){
+    let int = 1.15 ;
     let cuotas = parseInt(prompt("ingrese la cantidad de cuotas [1-12]"));
     if (!isNaN(cuotas) && cuotas >= 1 && cuotas <= 12) {
         let precioCuota = parseInt((precioTotal / cuotas) * int) ;
         let montoTotal = precioCuota * cuotas ;
-        alert(`El plan de pago es de ${cuotas} cuotas de : ${precioCuota} ARS finales por mes \n\ Monto total : ${montoTotal} ARS.`);
+        alert(`El plan de pago es de ${cuotas} cuotas de : ${precioCuota} ARS finales por mes \n\ Monto total : ${montoTotal} ARS. \n\ Tasa de interes : ${Math.round((int-1)*100)} %`);
     } else {
         alert("Las cuotas ingresadas son invalidas");
+        pagocuotas(precioTotal);
     }
+}
+// La funcion mostrarCarrito mediante un metodo forEach recorre el array y genera un console.log de sus objetos 
+function mostrarCarrito(carrito){
+console.log(`======* ORDEN DE COMPRA *======`);
+carrito.forEach(element => {
+console.log("=================");
+console.log("Producto: " + element.nombre);
+console.log("Precio: " + element.precio + " ARS");
+
+});
+console.log("=================");
+}
+// La funcion pagocash utiliza map para generar un nuevo array que tenga el precio de los productos reducidos en un 15% (descuento en efectivo) y los muestra mediante funcion mostrarCarrito(preciosEfectivo)
+function pagocash(carrito){
+const preciosEfectivo = carrito.map((elemento) => {
+return{
+    nombre:elemento.nombre,
+    precio:elemento.precio * 0.85
+}
+})
+mostrarCarrito(preciosEfectivo);
+console.log("El descuento en efectivo , ya ha sido aplicado");
 }
 
 //Entrada de datos del usuario
@@ -59,12 +99,17 @@ if (op===1){
 else {
     if(op===2){
         alert("Bienvenido al carrito de compras");
-        let total = comprarProductos();
-        let int = 1.1 ;
-        alert(`Usted ha sumado un total de : ${total} ARS`);
-        c=prompt(`Si desea pagar en cuotas pulse C , de lo contrario pulse cualquier tecla para salir `);
-        if (c == "c" || c == "C"){
-            pagocuotas(total,int);
+        comprarProductos();
+        mostrarCarrito(carrito);
+        let precioTotal= total(carrito);
+        console.log(precioTotal);
+        let pago = parseInt(prompt(`Si desea realizar el pago con TARJETA pulse "1"  \n\ si desea realizar el pago en EFECTIVO pulse "2" (Contamos con un 15% de descuento en efectivo) `));
+        switch(pago){
+            case 1: pagocuotas(precioTotal);
+            break;
+            case 2: pagocash(carrito);
+            break;
+            default: pago = parseInt(prompt(`Si desea realizar el pago con TARJETA pulse "1"  \n\ si desea realizar el pago en EFECTIVO pulse "2" (Contamos con un 15% de descuento en efectivo) `));
         }
     }
 }
